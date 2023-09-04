4d71a65f1fd5d9e421286122e85af4fe4c7348bf
Rbr4t <robertkoor0@gmail.com>
2023-09-04 22:02:58 +0300

    simple form system done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', messages: messages });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Express'})
+})
+
+router.post('/new', function(req, res, next) {
+  const data = req.body
+  messages.push({text: data.text, user: data.name, added: new Date()})
+  res.redirect('/')
+})
 
 module.exports = router;
